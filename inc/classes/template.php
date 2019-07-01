2341eafaf8d19bd85aa2e1a51cafdaf50c108140
Christopher Banks <oneeyefams@yahoo.com>
2019-07-01 18:33:10 -0400

    Updated template.php using the Singleton pattern  to closer match the requirements of #54

--- inc/classes/template.php
+++ inc/classes/template.php
@@ -1,13 +1,25 @@
 <?php
 
+// TEMPLATE Class is a singleton that can be statically invoked and will
+// store a chache of loaded template files for rendering
 class TEMPLATE
 {
-    public $filename="";
+    private static $TEMPLATE_ARR = [];
+
+    private $filename="";
     private $templateData="";
-    private $renderData="";
 
-    public function __construct($_filename){
+    private function __construct($_filename){
+        $this->filename = $_filename;
+        $this->templateData = file_get_contents($this->filename);
+    }
 
+    // Using the loaded Template String replace with the provided properties
+    // set $this->render with the finished rendering of the template
+    public static function render($_filename, $_props){
+        $template = null;
+
+        // normalize the filename
         $file_parts = pathinfo($_filename);
         switch($file_parts['extension']){
             case "":
@@ -15,22 +27,24 @@ class TEMPLATE
             break;
         }
 
-        $this->filename = $_filename;
+        // load from cache if already loaded otherwise, create template object
+        if(array_key_exists($_filename, self::$TEMPLATE_ARR)){
+            $template = self::$TEMPLATE_ARR[$_filename];
+        } else {
+            $template = new TEMPLATE($_filename);
+            array_push(self::$TEMPLATE_ARR, [$_filename => $template]);
+        }
 
-        $this->loadTemplate();
-    }
+        // return if template retrieval fails
+        if($template == null){
+            return null;
+        }
 
-    public function loadTemplate(){
-        $this->templateData = file_get_contents($this->filename);
-    }
-
-    // Using the loaded Template String replace with the provided properties
-    // set $this->render with the finished rendering of the template
-    public function render($_props){
-        if(trim($this->templateData) !== ''){
+        // If the template is loaded and valid atempt to insert values
+        if(trim($template->templateData) !== ''){
 
             // Set render for template starting point
-            $renderData = $this->templateData;
+            $renderData = $template->templateData;
             foreach($_props as $key => $value){
                 $pattern = '/{{'.$key.'}}/';
                 $renderData = preg_replace($pattern, $value, $renderData);
