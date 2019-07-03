4751d1398e3166e8b9380a7382df25be388719e5
seanjohnson08 <seanjohnson08@gmail.com>
2019-07-03 18:39:44 -0400

    Fix api routes after DI pattern

--- node-server/routes.js
+++ node-server/routes.js
@@ -1,12 +1,5 @@
 const Router = require('koa-router');
 const { inject } = require('./injections');
-const CategoriesResource = require('./resources/categories');
-const ForumsResource = require('./resources/forums');
-const MemberGroupsResource = require('./resources/member_groups');
-const MembersResource = require('./resources/members');
-const SessionsResource = require('./resources/sessions');
-const StatsResource = require('./resources/stats');
-const TopicsResource = require('./resources/topics');
 
 module.exports = function routes() {
   const router = new Router();
@@ -23,17 +16,17 @@ module.exports = function routes() {
 
   // api
   const apiRoutes = new Router();
-  apiRoutes.get('/categories', findAll(CategoriesResource));
-  apiRoutes.get('/category/:id', find(CategoriesResource));
-  apiRoutes.get('/forums', findAll(ForumsResource));
-  apiRoutes.get('/forum/:id', find(ForumsResource));
-  apiRoutes.get('/member_groups', findAll(MemberGroupsResource));
-  apiRoutes.get('/members', findAll(MembersResource));
-  apiRoutes.get('/member/:id', find(MembersResource));
-  apiRoutes.get('/sessions', findAll(SessionsResource));
-  apiRoutes.get('/stats', findAll(StatsResource));
-  apiRoutes.get('/topics', findAll(TopicsResource));
-  apiRoutes.get('/topic/:id', find(TopicsResource));
+  apiRoutes.get('/categories', findAll(inject('resources/categories')));
+  apiRoutes.get('/category/:id', find(inject('resources/categories')));
+  apiRoutes.get('/forums', findAll(inject('resources/forums')));
+  apiRoutes.get('/forum/:id', find(inject('resources/forums')));
+  apiRoutes.get('/member_groups', findAll(inject('resources/member_groups')));
+  apiRoutes.get('/members', findAll(inject('resources/members')));
+  apiRoutes.get('/member/:id', find(inject('resources/members')));
+  apiRoutes.get('/sessions', findAll(inject('resources/sessions')));
+  apiRoutes.get('/stats', findAll(inject('resources/stats')));
+  apiRoutes.get('/topics', findAll(inject('resources/topics')));
+  apiRoutes.get('/topic/:id', find(inject('resources/topics')));
 
   // Top level routes
   router.use('/api', apiRoutes.routes(), apiRoutes.allowedMethods());
