02af815844e1425df138fd2f0d77b8e221d1398e
seanjohnson08 <seanjohnson08@gmail.com>
2019-07-03 15:46:58 -0400

    Split node-server dependencies from main app. Also committing dependency .lock files, and changing app state markers to ".locked" to not conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   rules: {
     // Disables the rule preventing modifying properties on objects passed in
     'no-param-reassign': [2, { props: false }],
-    'prettier/prettier': ['error', { singleQuote: true }]
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'prefer-rest-params': 0
   },
 
   env: {
