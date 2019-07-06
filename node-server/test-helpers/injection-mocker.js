4d40d41b65f3a6c3f072535acd799c6f488bbe29
seanjohnson08 <seanjohnson08@gmail.com>
2019-07-06 10:44:23 -0400

    Fix tests after injection changes

--- node-server/test-helpers/injection-mocker.js
+++ node-server/test-helpers/injection-mocker.js
@@ -1,4 +1,9 @@
-const { inject } = require('../injections');
+const { inject, loadAll, register } = require('../injections');
+const routes = require('../routes');
+
+// Load all injections and do additional registrations
+loadAll();
+register('router', routes());
 
 module.exports = function injectionMocker(injections = {}) {
   return path => injections[path] || inject(path);
