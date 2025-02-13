cb511405b4e4e376939d61767d587820cc67eb2f
Sean Johnson <seanjohnson08@gmail.com>
2025-02-12 22:00:17 -0500

    Update dist.js

--- dist/app.js
+++ dist/app.js
@@ -122,8 +122,7 @@
     const yday = new Date();
     yday.setTime(yday - 1000 * 60 * 60 * 24);
 
-    const serverAsLocalDate = new Date(0);
-    serverAsLocalDate.setUTCSeconds(gmtUnixTimestamp);
+    const serverAsLocalDate = new Date((gmtUnixTimestamp - localTimeNow.getTimezoneOffset() * 60) * 1000);
 
     const deltaInSeconds = (localTimeNow - serverAsLocalDate) / 1000;
 
