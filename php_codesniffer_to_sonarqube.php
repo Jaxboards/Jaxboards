2daf277a5348d4283e9bafef5ea1cdb629d7e89a
World's Tallest Ladder <wtl420@users.noreply.github.com>
2025-03-23 12:16:15 -0700

    Revert "Apply automatic changes"
    This reverts commit 763e820955e224c373e7bcd0e7ceed32fc625237.

--- php_codesniffer_to_sonarqube.php
+++ php_codesniffer_to_sonarqube.php
@@ -165,9 +165,10 @@ file_put_contents(
                                     // of information from PHP_CodeSniffer so we
                                     // just add 1 to the starting positions
                                     // (otherwise SonarQube crashes)
-                                    'endColumn' => (string) (
-                                        ((int) $message['column']) - 1
-                                    ),
+                                    'endColumn'
+                                        => (string) (
+                                            ((int) $message['column']) - 1
+                                        ),
                                     'endLine' => (string) (
                                         (int) $message['line'] + 1
                                     ),
