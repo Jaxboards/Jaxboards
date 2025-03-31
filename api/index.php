11c1cf1655394a2f182548eeba79588fe753b982
Sean Johnson <seanjohnson08@gmail.com>
2025-03-31 10:45:58 -0400

    Fix crash and bug on admin themes page

---
+++ api/index.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../config.php';
+
+require __DIR__ . '/../inc/classes/mysql.php';
+$DB = new MySQL();
+$DB->connect(
+    $CFG['sql_host'],
+    $CFG['sql_username'],
+    $CFG['sql_password'],
+    $CFG['sql_db'],
+);
+
+require __DIR__ . '/../domaindefinitions.php';
+require __DIR__ . '/../inc/classes/jax.php';
+
+$list = [[], []];
+
+switch ($_GET['act'] ?? '') {
+    case 'searchmembers':
+        $result = $DB->safeselect(
+            [
+                'id',
+                'display_name',
+            ],
+            'members',
+            'WHERE `display_name` LIKE ? ORDER BY `display_name` LIMIT 10',
+            $DB->basicvalue(
+                htmlspecialchars(
+                    str_replace('_', '\_', $_GET['term']),
+                    ENT_QUOTES,
+                ) . '%',
+            ),
+        );
+        while ($f = $DB->arow($result)) {
+            $list[0][] = $f['id'];
+            $list[1][] = $f['display_name'];
+        }
+        echo json_encode($list);
+
+        break;
+
+    case 'emotes':
+            $JAX = new JAX();
+            $rules = $JAX->getEmoteRules(0);
+            foreach ($rules as $k => $v) {
+                $rules[$k] = '<img src="' . $v . '" alt="' . $JAX->blockhtml($k) . '" />';
+            }
+
+            echo json_encode([array_keys($rules), array_values($rules)]);
+            break;
+
+    default:
+}
+
