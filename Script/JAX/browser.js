c82ae51a1ff8f22c4b9c5bd67fcb2227c65a6b1c
Sean Johnson <seanjohnson08@gmail.com>
2025-04-03 02:11:32 -0400

    sonarqcube: dont use match

--- Script/JAX/browser.js
+++ Script/JAX/browser.js
@@ -1,11 +1,11 @@
 const { userAgent } = navigator;
 
 export default {
-  chrome: !!userAgent.match(/chrome/i),
-  ie: !!userAgent.match(/msie/i),
-  iphone: !!userAgent.match(/iphone/i),
-  mobile: !!userAgent.match(/mobile/i),
-  n3ds: !!userAgent.match(/nintendo 3ds/),
-  firefox: !!userAgent.match(/firefox/i),
-  safari: !!userAgent.match(/safari/i),
+  chrome: /chrome/i.test(userAgent),
+  ie: /msie/i.test(userAgent),
+  iphone: /iphone/i.test(userAgent),
+  mobile: /mobile/i.test(userAgent),
+  n3ds: /nintendo 3ds/.test(userAgent),
+  firefox: /firefox/i.test(userAgent),
+  safari: /safari/i.test(userAgent),
 };
