d2cec30c10795f798083182fc434298e94811a02
Sean Johnson <seanjohnson08@gmail.com>
2025-04-18 22:41:00 -0400

    katamari cors

--- inc/page/katamari.php
+++ inc/page/katamari.php
@@ -3,7 +3,7 @@ class Katamari {
     public function route() {
         global $PAGE;
 
-        $PAGE->JS("script","var i,s,ss=['./Script/katamari.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);");
+        $PAGE->JS("script","var i,s,ss=['./Script/katamari.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);");
         $PAGE->JS("softurl");
     }
 }
