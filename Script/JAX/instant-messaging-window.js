bbb519259c29d8164b8606bf84ee9742fa938175
Sean Johnson <seanjohnson08@gmail.com>
2025-04-22 03:49:43 -0400

    convert unnecessary class to function

--- Script/JAX/instant-messaging-window.js
+++ Script/JAX/instant-messaging-window.js
@@ -1,12 +1,10 @@
 /* global RUN, globalsettings */
 
-export default class IMWindow {
-  constructor(uid, uname) {
-    if (!globalsettings.can_im) {
-      // eslint-disable-next-line no-alert
-      alert('You do not have permission to use this feature.');
-    } else {
-      RUN.stream.commands.im([uid, uname, false]);
-    }
+export default function IMWindow (uid, uname) {
+  if (!globalsettings.can_im) {
+    // eslint-disable-next-line no-alert
+    alert('You do not have permission to use this feature.');
+  } else {
+    RUN.stream.commands.im([uid, uname, false]);
   }
 }
