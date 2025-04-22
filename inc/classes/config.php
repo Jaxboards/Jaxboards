043ac9c532be10629234ed7de1d6cdff3280bbed
Sean Johnson <seanjohnson08@gmail.com>
2025-04-22 06:41:14 -0400

    Update gitignore to only ignore service config

---
+++ inc/classes/config.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+class Config {
+    static function get() {
+        $config = array_merge(self::getServiceConfig(), self::getBoardConfig(), self::override());
+
+        return $config;
+    }
+
+    static function getServiceConfig() {
+        static $serviceConfig = null;
+
+        if ($serviceConfig) return $serviceConfig;
+
+        require JAXBOARDS_ROOT . '/config.php';
+
+        $serviceConfig = $CFG;
+
+        return $serviceConfig;
+    }
+
+    static function getBoardConfig($write = null) {
+        static $boardConfig = null;
+
+        if ($write) $boardConfig = array_merge($boardConfig, $write);
+
+        if ($boardConfig) return $boardConfig;
+
+        if (!defined('BOARDPATH')) {
+            $boardConfig = ['noboard' => 1];
+            return $boardConfig;
+        }
+
+        require BOARDPATH . '/config.php';
+
+        $boardConfig = $CFG;
+
+        return $boardConfig;
+    }
+
+    static function getSetting(string $key) {
+        $config = self::get();
+
+        if (array_key_exists($key, $config)) {
+            return $config[$key];
+        }
+
+        return null;
+    }
+
+    static function override($override = null) {
+        static $overrideConfig = [];
+
+        if ($override) {
+            $overrideConfig = $override;
+        }
+
+        return $overrideConfig;
+    }
+
+    static private function configFileContents($data) {
+        $dataString = json_encode($data, JSON_PRETTY_PRINT);
+
+        return <<<EOT
+            <?php
+            /**
+             * JaxBoards config file. It's just JSON embedded in PHP- wow!
+             *
+             * PHP Version 5.3.0
+             *
+             * @category Jaxboards
+             * @package  Jaxboards
+             *
+             * @author  Sean Johnson <seanjohnson08@gmail.com>
+             * @author  World's Tallest Ladder <wtl420@users.noreply.github.com>
+             * @license MIT <https://opensource.org/licenses/MIT>
+             *
+             * @link https://github.com/Jaxboards/Jaxboards Jaxboards on Github
+             */
+            \$CFG = json_decode(
+            <<<'EOD'
+            {$dataString}
+            EOD
+                ,
+                true
+            );
+            EOT;
+    }
+
+    static public function write($data)
+    {
+        $boardConfig = Config::getBoardConfig($data);
+
+        if (!defined('BOARDPATH')) throw new Exception('Board config file not determinable');
+
+        file_put_contents(BOARDPATH . 'config.php', self::configFileContents($boardConfig));
+    }
+
+    // Only used during installation
+    static function writeServiceConfig($data) {
+        file_put_contents(JAXBOARDS_ROOT . '/config.php', self::configFileContents($data));
+    }
+}
+?>
