fda1949fb57b42f84e64b89264e38a401a74a076
Sean Johnson <seanjohnson08@gmail.com>
2025-04-24 23:34:34 -0400

    Phpstan should use autoloader

--- tools/phpstan-bootstrap.php
+++ tools/phpstan-bootstrap.php
@@ -5,22 +5,8 @@ declare(strict_types=1);
 /*
  * Bootstrap file for phpstan and rector.
  *
- * Ensures that custom autoloading and other mechanisms are in palce to aid the
+ * Ensures that custom autoloading and other mechanisms are in place to aid the
  * static analysis
  */
 
-// phpcs:disable SlevomatCodingStandard.Variables.UnusedVariable.UnusedVariable
-$CFG = [];
-
-// always load configuration
-require_once dirname(__DIR__) . '/config.default.php';
-
-// load classes
-$classFiles = glob(dirname(__DIR__) . '/jax/*.php');
-
-// Exclude tests
-$classFiles = array_filter($classFiles, static fn($fileName): bool => !str_contains($fileName, '.test.'));
-
-foreach ($classFiles as $classFile) {
-    require_once $classFile;
-}
+require_once dirname(__DIR__) . '/jax/autoload.php';
