b386a21e37ac86353424c4c395a188782b1180ec
Sean Johnson <seanjohnson08@gmail.com>
2025-04-25 06:19:30 -0400

    make acp login page classy

--- acp/index.php
+++ acp/index.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use ACP\Page;
+use ACP\Page\Login;
 use DI\Container;
-use Jax\Config;
-use Jax\Database;
-use Jax\Jax;
-use Jax\User;
+
+if (!defined('JAXBOARDS_ROOT')) {
+    define('JAXBOARDS_ROOT', dirname(__DIR__));
+}
 
 /*
  * Admin login.
@@ -16,15 +16,6 @@ use Jax\User;
  *
  * @see https://github.com/Jaxboards/Jaxboards Jaxboards Github repo
  */
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_cookies', 1);
-ini_set('session.use_only_cookies', 1);
-session_start();
-
-if (!defined('JAXBOARDS_ROOT')) {
-    define('JAXBOARDS_ROOT', dirname(__DIR__));
-}
 
 // Load composer dependencies.
 require_once JAXBOARDS_ROOT . '/jax/autoload.php';
@@ -32,71 +23,4 @@ $container = new Container();
 
 require_once JAXBOARDS_ROOT . '/domaindefinitions.php';
 
-$CFG = $container->get(Config::class)->get();
-
-$DB = $container->get(Database::class);
-$DB->connect(
-    $CFG['sql_host'],
-    $CFG['sql_username'],
-    $CFG['sql_password'],
-    $CFG['sql_db'],
-    $CFG['sql_prefix'],
-);
-
-
-$JAX = $container->get(Jax::class);
-$PAGE = $container->get(Page::class);
-$USERCLASS = $container->get(User::class);
-
-$themeElements = [
-    'board_name' => $CFG['boardname'],
-    'board_url' => BOARDURL,
-    'content' => '',
-    'css_url' => BOARDURL . 'acp/css/login.css',
-    'favicon_url' => BOARDURL . 'favicon.ico',
-];
-
-if (isset($JAX->p['submit'])) {
-    $user = $JAX->p['user'];
-    $password = $JAX->p['pass'];
-
-    $result = $DB->safespecial(
-        <<<'EOT'
-            SELECT
-                m.`id` as `id`,
-                g.`can_access_acp` as `can_access_acp`
-            FROM %t m
-                LEFT JOIN %t g ON m.`group_id` = g.`id`
-                WHERE m.`name`=?;
-            EOT
-        ,
-        ['members', 'member_groups'],
-        $DB->basicvalue($user),
-    );
-    $uinfo = $DB->arow($result);
-    $DB->disposeresult($result);
-
-    if (!$uinfo) {
-        $themeElements['content'] = $PAGE->error(
-            'The username/password supplied was incorrect',
-        );
-    } elseif (is_array($uinfo)) {
-        // Check password.
-        $verified_password = (bool) $USERCLASS->getUser($uinfo['id'], $password);
-
-        if ($uinfo['can_access_acp'] && $verified_password) {
-            $_SESSION['auid'] = $uinfo['id'];
-            // Successful login, redirect
-            header('Location: admin.php');
-        } else {
-            $themeElements['content'] = $PAGE->error(
-                'You are not authorized to log in to the ACP',
-            );
-        }
-    }
-}
-
-echo $PAGE->parseTemplate(
-    'login.html',
-    $themeElements,
-);
+$container->get(Login::class)->render();
---
+++ acp/page/login.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ACP\Page;
+
+use ACP\Page;
+use Jax\Config;
+use Jax\Database;
+use Jax\Jax;
+use Jax\User;
+
+class Login {
+    public function __construct(
+        private readonly Config $config,
+        private readonly Database $database,
+        private readonly Jax $jax,
+        private readonly Page $page,
+        private readonly User $user,
+    ) {}
+    private function sessionStart() {
+        ini_set('session.cookie_secure', 1);
+        ini_set('session.cookie_httponly', 1);
+        ini_set('session.use_cookies', 1);
+        ini_set('session.use_only_cookies', 1);
+        session_start();
+    }
+    public function render() {
+        $this->database->connect(
+            $this->config->getSetting('sql_host'),
+            $this->config->getSetting('sql_username'),
+            $this->config->getSetting('sql_password'),
+            $this->config->getSetting('sql_db'),
+            $this->config->getSetting('sql_prefix'),
+        );
+
+        $pageElements = [
+            'board_name' => $this->config->getSetting('boardname'),
+            'board_url' => BOARDURL,
+            'content' => '',
+            'css_url' => BOARDURL . 'acp/css/login.css',
+            'favicon_url' => BOARDURL . 'favicon.ico',
+        ];
+
+        if (isset($this->jax->p['submit'])) {
+            $user = $this->jax->p['user'];
+            $password = $this->jax->p['pass'];
+
+            $result = $this->database->safeselect(
+                ['id'],
+                'members',
+                'WHERE `name`=?',
+                $user
+            );
+            $member = $this->database->row($result);
+            $user = $member ? $this->user->getUser($member['id'], $password) : null;
+            $this->database->disposeresult($result);
+
+            if ($user === null) {
+                $pageElements['content'] = $this->page->error(
+                    'The username/password supplied was incorrect',
+                );
+            } elseif (!$this->user->getPerm('can_access_acp')) {
+                $pageElements['content'] = $this->page->error(
+                    'You are not authorized to log in to the ACP',
+                );
+            } else {
+                $_SESSION['auid'] = $user['id'];
+                // Successful login, redirect
+                header('Location: admin.php');
+            }
+        }
+
+        echo $this->page->parseTemplate(
+            'login.html',
+            $pageElements,
+        );
+    }
+}
