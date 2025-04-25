3c10af8b196cfbee1943335ec850f9102aeb8788
Sean Johnson <seanjohnson08@gmail.com>
2025-04-25 06:47:49 -0400

    classify admin application

--- acp/admin.php
+++ acp/admin.php
@@ -2,30 +2,21 @@
 
 declare(strict_types=1);
 
-use ACP\Page;
+use ACP\App;
 use DI\Container;
-use Jax\Config;
-use Jax\Database;
 use Jax\DomainDefinitions;
-use Jax\Jax;
-use Jax\User;
 
-/*
- * Admin control panel.
+if (!defined('JAXBOARDS_ROOT')) {
+    define('JAXBOARDS_ROOT', dirname(__DIR__));
+}
+
+/**
+ * Admin login.
  *
  * PHP Version 5.3.7
  *
  * @see https://github.com/Jaxboards/Jaxboards Jaxboards Github repo
  */
-ini_set('session.cookie_secure', 1);
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_cookies', 1);
-ini_set('session.use_only_cookies', 1);
-session_start();
-
-if (!defined('JAXBOARDS_ROOT')) {
-    define('JAXBOARDS_ROOT', dirname(__DIR__));
-}
 
 // Load composer dependencies.
 require_once JAXBOARDS_ROOT . '/jax/autoload.php';
@@ -33,107 +24,4 @@ $container = new Container();
 
 $container->get(DomainDefinitions::class)->defineConstants();
 
-$CFG = $container->get(Config::class)->get();
-
-$DB = $container->get(Database::class);
-$DB->connect(
-    $CFG['sql_host'],
-    $CFG['sql_username'],
-    $CFG['sql_password'],
-    $CFG['sql_db'],
-    $CFG['sql_prefix'],
-);
-$USER = $container->get(User::class);
-
-$JAX = $container->get(Jax::class);
-
-if (isset($_SESSION['auid'])) {
-    $userData = $USER->getUser($_SESSION['auid']);
-}
-
-if (!$USER->getPerm('can_access_acp')) {
-    header('Location: ./');
-
-    exit;
-}
-
-$PAGE = $container->get(Page::class);
-$PAGE->append('username', $USER->get('display_name'));
-$PAGE->title($CFG['boardname'] . ' - ACP');
-$PAGE->addNavMenu(
-    'Settings',
-    '?act=settings',
-    [
-        '?act=settings&do=birthday' => 'Birthdays',
-        '?act=settings&do=global' => 'Global Settings',
-        '?act=settings&do=pages' => 'Custom Pages',
-        '?act=settings&do=shoutbox' => 'Shoutbox',
-    ],
-);
-$PAGE->addNavMenu(
-    'Members',
-    '?act=members',
-    [
-        '?act=members&do=delete' => 'Delete Account',
-        '?act=members&do=edit' => 'Edit',
-        '?act=members&do=ipbans' => 'IP Bans',
-        '?act=members&do=massmessage' => 'Mass Message',
-        '?act=members&do=merge' => 'Account Merge',
-        '?act=members&do=prereg' => 'Pre-Register',
-        '?act=members&do=validation' => 'Validation',
-    ],
-);
-$PAGE->addNavMenu(
-    'Groups',
-    '?act=groups',
-    [
-        '?act=groups&do=create' => 'Create Group',
-        '?act=groups&do=delete' => 'Delete Groups',
-        '?act=groups&do=perms' => 'Edit Permissions',
-    ],
-);
-$PAGE->addNavMenu(
-    'Themes',
-    '?act=themes',
-    [
-        '?act=themes&do=create' => 'Create Skin',
-        '?act=themes' => 'Manage Skin(s)',
-    ],
-);
-$PAGE->addNavMenu(
-    'Posting',
-    '?act=posting',
-    [
-        '?act=posting&do=emoticons' => 'Emoticons',
-        '?act=posting&do=postrating' => 'Post Rating',
-        '?act=posting&do=wordfilter' => 'Word Filter',
-    ],
-);
-$PAGE->addNavMenu(
-    'Forums',
-    '?act=forums',
-    [
-        '?act=forums&do=create' => 'Create Forum',
-        '?act=forums&do=createc' => 'Create Category',
-        '?act=forums&do=order' => 'Manage',
-        '?act=forums&do=recountstats' => 'Recount Statistics',
-    ],
-);
-$PAGE->addNavMenu(
-    'Tools',
-    '?act=tools',
-    [
-        '?act=tools&do=backup' => 'Backup Forum',
-        '?act=tools&do=files' => 'File Manager',
-        '?act=tools&do=errorlog' => 'View Error Log',
-    ],
-);
-
-$act = $JAX->g['act'] ?? null;
-
-if ($act && file_exists("./page/{$act}.php")) {
-    $page = $container->get('ACP\Page\\' . $act);
-    $page->render();
-}
-
-$PAGE->out();
+$container->get(App::class)->render();
---
+++ acp/app.php
@@ -0,0 +1,146 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ACP;
+
+use ACP\Page;
+use DI\Container;
+use Jax\Config;
+use Jax\Database;
+use Jax\DomainDefinitions;
+use Jax\Jax;
+use Jax\User;
+
+/*
+ * Admin control panel.
+ *
+ * PHP Version 5.3.7
+ *
+ * @see https://github.com/Jaxboards/Jaxboards Jaxboards Github repo
+ */
+
+class App {
+    public function __construct(
+        private readonly Config $config,
+        private readonly Container $container,
+        private readonly Database $database,
+        private readonly Jax $jax,
+        private readonly Page $page,
+        private readonly User $user,
+    ) {}
+
+    private function startSession(): void {
+        ini_set('session.cookie_secure', 1);
+        ini_set('session.cookie_httponly', 1);
+        ini_set('session.use_cookies', 1);
+        ini_set('session.use_only_cookies', 1);
+        session_start();
+    }
+
+    private function connectDB() {
+        $this->database->connect(
+            $this->config->getSetting('sql_host'),
+            $this->config->getSetting('sql_username'),
+            $this->config->getSetting('sql_password'),
+            $this->config->getSetting('sql_db'),
+            $this->config->getSetting('sql_prefix'),
+        );
+    }
+
+    public function render() {
+        $this->startSession();
+
+        $this->connectDB();
+
+        if (isset($_SESSION['auid'])) {
+            $userData = $this->user->getUser($_SESSION['auid']);
+        }
+
+        if (!$this->user->getPerm('can_access_acp')) {
+            header('Location: ./');
+
+            exit;
+        }
+
+        $this->page->append('username', $this->user->get('display_name'));
+        $this->page->title($this->config->getSetting('boardname') . ' - ACP');
+        $this->page->addNavMenu(
+            'Settings',
+            '?act=settings',
+            [
+                '?act=settings&do=birthday' => 'Birthdays',
+                '?act=settings&do=global' => 'Global Settings',
+                '?act=settings&do=pages' => 'Custom Pages',
+                '?act=settings&do=shoutbox' => 'Shoutbox',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Members',
+            '?act=members',
+            [
+                '?act=members&do=delete' => 'Delete Account',
+                '?act=members&do=edit' => 'Edit',
+                '?act=members&do=ipbans' => 'IP Bans',
+                '?act=members&do=massmessage' => 'Mass Message',
+                '?act=members&do=merge' => 'Account Merge',
+                '?act=members&do=prereg' => 'Pre-Register',
+                '?act=members&do=validation' => 'Validation',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Groups',
+            '?act=groups',
+            [
+                '?act=groups&do=create' => 'Create Group',
+                '?act=groups&do=delete' => 'Delete Groups',
+                '?act=groups&do=perms' => 'Edit Permissions',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Themes',
+            '?act=themes',
+            [
+                '?act=themes&do=create' => 'Create Skin',
+                '?act=themes' => 'Manage Skin(s)',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Posting',
+            '?act=posting',
+            [
+                '?act=posting&do=emoticons' => 'Emoticons',
+                '?act=posting&do=postrating' => 'Post Rating',
+                '?act=posting&do=wordfilter' => 'Word Filter',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Forums',
+            '?act=forums',
+            [
+                '?act=forums&do=create' => 'Create Forum',
+                '?act=forums&do=createc' => 'Create Category',
+                '?act=forums&do=order' => 'Manage',
+                '?act=forums&do=recountstats' => 'Recount Statistics',
+            ],
+        );
+        $this->page->addNavMenu(
+            'Tools',
+            '?act=tools',
+            [
+                '?act=tools&do=backup' => 'Backup Forum',
+                '?act=tools&do=files' => 'File Manager',
+                '?act=tools&do=errorlog' => 'View Error Log',
+            ],
+        );
+
+        $act = $this->jax->g['act'] ?? null;
+
+        if ($act && file_exists("./page/{$act}.php")) {
+            $page = $this->container->get('ACP\Page\\' . $act);
+            $page->render();
+        }
+
+        $this->page->out();
+    }
+}
