b921c533bb6efe058ec3dfa5542d295107e2a41e
Sean Johnson <seanjohnson08@gmail.com>
2025-04-29 18:14:01 -0400

    array_any is only 8.4 dangit

--- Jax/autoload.php
+++ Jax/autoload.php
@@ -13,6 +13,20 @@ use function str_replace;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+// Polyfills
+if (!function_exists('array_any')) {
+    // Only exists in 8.4 :(
+    function array_any(array $array, callable $callback): bool
+    {
+        foreach($array as $v) {
+            if ($callback($v)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 spl_autoload_register(static function ($className): void {
     $classPath = dirname(__DIR__) . '/' . str_replace('\\', '/', $className) . '.php';
 
