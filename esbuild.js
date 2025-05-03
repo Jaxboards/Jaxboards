4e477eb575a40ae014b962b34438fea58302f874
Sean Johnson <seanjohnson08@gmail.com>
2025-05-03 12:09:17 -0400

    Replace entire JS build process with esbuild to unlock typescript

---
+++ esbuild.js
@@ -0,0 +1,22 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+  entryPoints: ['Script/app.ts'],
+  bundle: true,
+  outfile: 'dist/app.js',
+  minify: true,
+})
+
+await esbuild.build({
+    entryPoints: ['Script/acp.ts'],
+    bundle: true,
+    outfile: 'dist/acp.js',
+    minify: true,
+})
+
+await esbuild.build({
+    entryPoints: ['Script/modcontrols.ts'],
+    bundle: true,
+    outfile: 'dist/modcontrols.js',
+    minify: true,
+})
