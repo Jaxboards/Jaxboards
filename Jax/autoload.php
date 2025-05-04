79851c948f1cc140dfbcf4a2d0b4f52414742119
World's Tallest Ladder <wtl420@users.noreply.github.com>
2025-05-03 20:45:52 -0700

    Revert "add exceptions to autoload to avoid exceptions"
    This reverts commit 9bf59ef89479afc443f8a18ffffab1a9fe583242.

--- Jax/autoload.php
+++ Jax/autoload.php
@@ -8,32 +8,15 @@ use Exception;
 
 use function dirname;
 use function file_exists;
-use function in_array;
 use function spl_autoload_register;
 use function str_replace;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 spl_autoload_register(static function ($className): void {
-    $relativeClassPath = '/' . str_replace('\\', '/', $className) . '.php';
-    $classPath = dirname(__DIR__) . $relativeClassPath;
+    $classPath = dirname(__DIR__) . '/' . str_replace('\\', '/', $className) . '.php';
 
     if (!file_exists($classPath)) {
-        if (
-            in_array(
-                $relativeClassPath,
-                [
-                    // rector attempts to load these classes errorneously and
-                    // errors are safe to ignore here
-                    '/PHPUnit/Framework/TestCase.php',
-                    '/null.php',
-                ],
-                true,
-            )
-        ) {
-            return;
-        }
-
         throw new Exception("Error loading class: {$classPath}");
     }
 
