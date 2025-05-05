4e7da7490e279a7271c89e1e958a42731930e52b
World's Tallest Ladder <wtl420@users.noreply.github.com>
2025-05-04 22:09:50 -0700

    get tests passing

--- tests/Jax/UserTest.php
+++ tests/Jax/UserTest.php
@@ -26,17 +26,26 @@ final class UserTest extends TestCase
 {
     private string $encodedForumFlags;
 
-    private Container $container;
-
     /**
      * @var array<int,array<string,bool>>
      */
     private array $decoded = [
-        1 => ['upload' => false, 'reply' => true, 'start' => true, 'read' => true, 'view' => true, 'poll' => true],
-        3 => ['upload' => false, 'reply' => false, 'start' => false, 'read' => true, 'view' => true, 'poll' => false],
-        4 => ['upload' => false, 'reply' => false, 'start' => false, 'read' => true, 'view' => true, 'poll' => false],
-        5 => ['upload' => false, 'reply' => false, 'start' => false, 'read' => true, 'view' => true, 'poll' => false],
-        6 => ['upload' => true, 'reply' => true, 'start' => true, 'read' => true, 'view' => true, 'poll' => true],
+        3 => [
+            'upload' => false,
+            'reply' => false,
+            'start' => false,
+            'read' => true,
+            'view' => true,
+            'poll' => false,
+        ],
+        4 => [
+            'upload' => false,
+            'reply' => false,
+            'start' => false,
+            'read' => true,
+            'view' => true,
+            'poll' => false,
+        ],
     ];
 
     protected function setUp(): void
@@ -44,20 +53,50 @@ final class UserTest extends TestCase
         parent::setUp();
 
         $this->encodedForumFlags = base64_decode('AAEAPgADABgABAAYAAUAGAAGAD8=', true);
-
-        $this->container = new Container();
     }
 
     public function testGetForumPermissionAsAdmin(): void
     {
-        $user = $this->getUser(
+        $groupID = 2;
+        // admin? where are these defined? these should be constants or an enum
+
+        $database = self::getMockBuilder(Database::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([
+                'arow',
+                'disposeresult',
+                'safeselect',
+            ])
+            ->getMock()
+        ;
+
+        $database->expects(self::never())
+                 ->method('safeselect')
+             ;
+
+        $database->expects(self::never())
+                 ->method('arow')
+             ;
+
+        $database->expects(self::never())
+                 ->method('disposeresult')
+             ;
+
+        $container = new Container([
+            Database::class => $database,
+        ]);
+
+        $user = new User(
+            $database,
+            $container->get(Jax::class),
+            $container->get(IPAddress::class),
+            ['group_id' => $groupID],
             [
                 'can_attach' => true,
                 'can_poll' => true,
                 'can_post' => true,
                 'can_post_topics' => true,
             ],
-            ['group_id' => 2],
         );
 
         self::assertSame(
@@ -75,54 +114,90 @@ final class UserTest extends TestCase
 
     public function testGetForumPermissionAsGuest(): void
     {
-        $user = $this->getUser(
+        $groupID = 3;
+        // guest? where are these defined? these should be constants or an enum
+
+        $database = self::getMockBuilder(Database::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods([
+                'arow',
+                'disposeresult',
+                'safeselect',
+            ])
+            ->getMock()
+        ;
+
+        $database->expects(self::never())
+                 ->method('safeselect')
+             ;
+
+        $database->expects(self::never())
+                 ->method('arow')
+             ;
+
+        $database->expects(self::never())
+                 ->method('disposeresult')
+             ;
+
+        $container = new Container([
+            Database::class => $database,
+        ]);
+
+        $user = new User(
+            $database,
+            $container->get(Jax::class),
+            $container->get(IPAddress::class),
+            ['group_id' => $groupID],
             ['can_post' => true],
-            ['group_id' => 3],
         );
 
         self::assertSame(
-            $this->decoded[3],
+            $this->decoded[$groupID],
             $user->getForumPerms($this->encodedForumFlags),
         );
     }
 
     public function testGetForumPermissionAsBanned(): void
     {
-        $user = $this->getUser(
-            ['can_post' => true],
-            ['group_id' => 4],
-        );
+        $groupID = 4;
+        // banned? where are these defined? these should be constants or an enum
 
-        $expected = $this->decoded[4];
-        $result = $user->getForumPerms($this->encodedForumFlags);
-        self::assertSame($expected, $result);
-    }
-
-    /**
-     * @param null|array<array-key,mixed> $userPerms
-     * @param null|array<array-key,mixed> $userData
-     */
-    private function getUser(?array $userPerms, ?array $userData): User
-    {
         $database = self::getMockBuilder(Database::class)
             ->disableOriginalConstructor()
             ->onlyMethods([
                 'arow',
-                'basicvalue',
                 'disposeresult',
                 'safeselect',
-                'safeupdate',
             ])
             ->getMock()
         ;
 
-        return new User(
+        $database->expects(self::never())
+                 ->method('safeselect')
+             ;
+
+        $database->expects(self::never())
+                 ->method('arow')
+             ;
+
+        $database->expects(self::never())
+                 ->method('disposeresult')
+             ;
+
+        $container = new Container([
+            Database::class => $database,
+        ]);
+
+        $user = new User(
             $database,
-            $this->container->get(Jax::class),
-            // I think this needs to mock the db too?
-            $this->container->get(IPAddress::class),
-            $userData,
-            $userPerms,
+            $container->get(Jax::class),
+            $container->get(IPAddress::class),
+            ['group_id' => $groupID],
+            ['can_post' => true],
         );
+
+        $expected = $this->decoded[$groupID];
+        $result = $user->getForumPerms($this->encodedForumFlags);
+        self::assertSame($expected, $result);
     }
 }
