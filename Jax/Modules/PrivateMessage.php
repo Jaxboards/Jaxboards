8cb1b0fa6ee14fffe88cc190079ee3d6485a6872
Sean Johnson <seanjohnson08@gmail.com>
2025-05-26 07:53:30 -0400

    fix access warning

--- Jax/Modules/PrivateMessage.php
+++ Jax/Modules/PrivateMessage.php
@@ -60,7 +60,7 @@ final readonly class PrivateMessage
         $commands = explode(PHP_EOL, (string) $this->session->get('runonce'));
         foreach ($commands as $index => $command) {
             $command = json_decode($command);
-            if ($command[0] !== 'im') {
+            if (!$command || $command[0] !== 'im') {
                 continue;
             }
 
