82d13acbe958996d70e6d88c57f3b56dd5a2b482
Sean Johnson <seanjohnson08@gmail.com>
2025-06-02 05:19:24 -0400

    few v3 script fixes

--- tools/migrations/V3/CreateBadgeTables.php
+++ tools/migrations/V3/CreateBadgeTables.php
@@ -6,22 +6,22 @@ namespace tools\migrations\V3;
 
 use Jax\Database;
 
-final class CreateBadgeTable
+final class CreateBadgeTables
 {
     public function execute(Database $database): void
     {
         $database->special(
-            'CREATE TABLE %t
+            "CREATE TABLE %t
                 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 `imagePath` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
                 `badgeTitle` VARCHAR(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'No Badge Title',
                 `displayOrder` INT NOT NULL,
-                `description` VARCHAR(255) COLLATE utf8mb4_unicode_ci NULL',
+                `description` VARCHAR(255) COLLATE utf8mb4_unicode_ci NULL",
             ['badges'],
         );
-	
+
         $database->special(
-            'CREATE TABLE %t
+            "CREATE TABLE %t
                 `id` SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 `user` INT NOT NULL,
                 `badge` INT NOT NULL,
@@ -30,9 +30,9 @@ final class CreateBadgeTable
                 `awardDate` DATETIME NULL,
                 KEY `user` (`user`),
                 KEY `badge` (`badge`),
-                CONSTRAINT `badge_associations_ibfk_1` FOREIGN KEY (`user`) REFERENCES `jaxboards_members` (`id`) ON DELETE SET NULL,
-                CONSTRAINT `badge_associations_ibfk_2` FOREIGN KEY (`badge`) REFERENCES `jaxboards_badges` (`id`) ON DELETE SET NULL'
-            ['badge_assocations'],
+                CONSTRAINT `badge_associations_ibfk_1` FOREIGN KEY (`user`) REFERENCES `%t` (`id`) ON DELETE SET NULL,
+                CONSTRAINT `badge_associations_ibfk_2` FOREIGN KEY (`badge`) REFERENCES `%t` (`id`) ON DELETE SET NULL",
+            ['badge_assocations', 'members', 'badges'],
         );
     }
 }
