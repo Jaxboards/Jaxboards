b301b82c9ca87080b6a36f585b1cad4492921fb9
Sean Johnson <seanjohnson08@gmail.com>
2025-06-15 21:17:59 -0400

    add keys and constraints

--- Jax/DatabaseUtils.php
+++ Jax/DatabaseUtils.php
@@ -3,6 +3,8 @@
 namespace Jax;
 
 use Jax\Attributes\Column;
+use Jax\Attributes\ForeignKey;
+use Jax\Attributes\Key;
 use Jax\Models\Post;
 use ReflectionClass;
 
@@ -19,24 +21,55 @@ class DatabaseUtils
         $reflectionClass = new ReflectionClass($modelClass::class);
 
         $fields = [];
+        $keys = [];
+        $constraints = [];
 
         foreach ($reflectionClass->getProperties() as $reflectionProperty) {
             $columnAttributes = $reflectionProperty->getAttributes(Column::class);
 
+            if ($columnAttributes === []) {
+                continue;
+            }
+
             $props = $columnAttributes[0]->newInstance();
 
-            $name = $props->name;
+            $fieldName = $this->database->quoteIdentifier($props->name);
             $type = $props->type;
             $length = $props->length !== 0 ? "({$props->length})" : '';
             $nullable = $props->nullable === false ? ' NOT NULL' : '';
             $unsigned = $props->unsigned === true ? ' unsigned' : '';
             $default = $props->default !== null ? " DEFAULT '{$props->default}'" : '';
 
-            $fields[] = "  `{$name}` {$type}{$length}{$unsigned}{$nullable}{$default}";
+            $fields[] = "{$fieldName} {$type}{$length}{$unsigned}{$nullable}{$default}";
+
+            $foreignKeyAttributes = $reflectionProperty->getAttributes(ForeignKey::class);
+            $keyAttributes = $reflectionProperty->getAttributes(Key::class);
+
+            if ($foreignKeyAttributes !== []) {
+                $foreignKey = $foreignKeyAttributes[0]->newInstance();
+                $foreignField = $this->database->quoteIdentifier($foreignKey->field);
+                $foreignTable = $this->database->ftable($foreignKey->table);
+                $onDelete = match($foreignKey->onDelete) {
+                    'cascade' => ' ON DELETE CASCADE',
+                    'null' => ' ON DELETE SET NULL',
+                    default => ''
+                };
+                $keys[] = "KEY {$fieldName} ({$fieldName})";
+                $constraints[] = "CONSTRAINT {$fieldName} FOREIGN KEY ({$fieldName}) REFERENCES $foreignTable ($foreignField){$onDelete}";
+            }
+
+            if ($keyAttributes !== []) {
+                $keys[] = "KEY {$fieldName} ({$fieldName})";
+            }
+
         }
 
         return "CREATE TABLE $tableName (" . PHP_EOL
-            . implode(',' . PHP_EOL, $fields) . PHP_EOL .
+            . '  ' . implode(',' . PHP_EOL . '  ', array_merge(
+                $fields,
+                $keys,
+                $constraints
+            )) . PHP_EOL .
         ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
     }
 
