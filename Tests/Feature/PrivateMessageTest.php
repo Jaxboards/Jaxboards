0d18d6a98a009bb279cf87858b2937934ca999bb
Sean Johnson <seanjohnson08@gmail.com>
2025-12-08 01:24:53 -0500

    PrivateMessageTest

---
+++ Tests/Feature/PrivateMessageTest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Jax\App;
+use Jax\Attributes\Column;
+use Jax\Attributes\ForeignKey;
+use Jax\Attributes\Key;
+use Jax\BBCode;
+use Jax\BotDetector;
+use Jax\Config;
+use Jax\Constants\JSAccess;
+use Jax\Database;
+use Jax\DatabaseUtils;
+use Jax\DatabaseUtils\SQLite;
+use Jax\Date;
+use Jax\DebugLog;
+use Jax\DomainDefinitions;
+use Jax\FileSystem;
+use Jax\IPAddress;
+use Jax\Jax;
+use Jax\Model;
+use Jax\Modules\PrivateMessage;
+use Jax\Modules\Shoutbox;
+use Jax\Page;
+use Jax\Page\IDX;
+use Jax\Page\TextRules;
+use Jax\Request;
+use Jax\RequestStringGetter;
+use Jax\Router;
+use Jax\ServiceConfig;
+use Jax\Session;
+use Jax\Template;
+use Jax\TextFormatting;
+use Jax\User;
+use Jax\UsersOnline;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\FeatureTestCase;
+
+/**
+ * @internal
+ */
+#[CoversClass(PrivateMessage::class)]
+#[CoversClass(App::class)]
+#[CoversClass(Column::class)]
+#[CoversClass(ForeignKey::class)]
+#[CoversClass(Key::class)]
+#[CoversClass(BBCode::class)]
+#[CoversClass(BotDetector::class)]
+#[CoversClass(Config::class)]
+#[CoversClass(Database::class)]
+#[CoversClass(DatabaseUtils::class)]
+#[CoversClass(SQLite::class)]
+#[CoversClass(Date::class)]
+#[CoversClass(DebugLog::class)]
+#[CoversClass(DomainDefinitions::class)]
+#[CoversClass(FileSystem::class)]
+#[CoversClass(IPAddress::class)]
+#[CoversClass(Jax::class)]
+#[CoversClass(Model::class)]
+#[CoversClass(Shoutbox::class)]
+#[CoversClass(Page::class)]
+#[CoversClass(IDX::class)]
+#[CoversClass(TextRules::class)]
+#[CoversClass(Request::class)]
+#[CoversClass(RequestStringGetter::class)]
+#[CoversClass(Router::class)]
+#[CoversClass(ServiceConfig::class)]
+#[CoversClass(Session::class)]
+#[CoversClass(Template::class)]
+#[CoversClass(TextFormatting::class)]
+#[CoversClass(User::class)]
+#[CoversClass(UsersOnline::class)]
+final class PrivateMessageTest extends FeatureTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testSendMessage(): void
+    {
+        $this->actingAs('admin');
+
+        $page = $this->go(new Request(
+            post: ['im_uid' => '1',  'im_im' => 'test'],
+            server: ['HTTP_X_JSACCESS' => JSAccess::ACTING->value],
+        ));
+
+        $json = json_decode($page, true);
+
+        $instantMessageCommand = array_find($json, fn($cmd): bool => $cmd[0] === 'im');
+        $this->assertEquals(1, $instantMessageCommand[1]);
+        $this->assertEquals('Admin', $instantMessageCommand[2]);
+        $this->assertEquals('test', $instantMessageCommand[3]);
+        $this->assertEquals(1, $instantMessageCommand[4]);
+    }
+}
