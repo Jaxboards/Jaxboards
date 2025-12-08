bc0081234ee4efa1b5615fddb6b845fb11bc336b
Sean Johnson <seanjohnson08@gmail.com>
2025-12-07 23:18:07 -0500

    add covers annotations

--- Tests/Feature/BadgesTest.php
+++ Tests/Feature/BadgesTest.php
@@ -4,17 +4,92 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
+use Jax\App;
+use Jax\Attributes\Column;
+use Jax\Attributes\ForeignKey;
+use Jax\Attributes\Key;
+use Jax\BBCode;
+use Jax\BotDetector;
+use Jax\Config;
+use Jax\ContactDetails;
+use Jax\DatabaseUtils;
+use Jax\DatabaseUtils\SQLite;
+use Jax\Date;
+use Jax\DebugLog;
+use Jax\DomainDefinitions;
+use Jax\FileSystem;
+use Jax\IPAddress;
+use Jax\Jax;
+use Jax\Modules\PrivateMessage;
+use Jax\Modules\Shoutbox;
+use Jax\Page;
+use Jax\Page\Badges;
+use Jax\Page\TextRules;
+use Jax\Page\UserProfile;
+use Jax\Page\UserProfile\Activity;
+use Jax\Page\UserProfile\Comments;
+use Jax\Page\UserProfile\ProfileTabs;
+use Jax\Request;
+use Jax\RequestStringGetter;
+use Jax\Router;
+use Jax\ServiceConfig;
+use Jax\Session;
+use Jax\Template;
+use Jax\TextFormatting;
+use Jax\User;
+use Jax\Page\Topic;
+use Jax\Page\Topic\Poll;
+use Jax\Page\Topic\Reactions;
+use Jax\UsersOnline;
 use Jax\Database;
 use Jax\Models\Badge;
 use Jax\Models\BadgeAssociation;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\DOMAssert;
 use Tests\FeatureTestCase;
 
 /**
  * @internal
- *
- * @coversNothing
  */
+#[CoversClass(Activity::class)]
+#[CoversClass(App::class)]
+#[CoversClass(Badge::class)]
+#[CoversClass(Badges::class)]
+#[CoversClass(BBCode::class)]
+#[CoversClass(BotDetector::class)]
+#[CoversClass(Column::class)]
+#[CoversClass(Comments::class)]
+#[CoversClass(Config::class)]
+#[CoversClass(ContactDetails::class)]
+#[CoversClass(Database::class)]
+#[CoversClass(DatabaseUtils::class)]
+#[CoversClass(Date::class)]
+#[CoversClass(DebugLog::class)]
+#[CoversClass(DomainDefinitions::class)]
+#[CoversClass(FileSystem::class)]
+#[CoversClass(ForeignKey::class)]
+#[CoversClass(IPAddress::class)]
+#[CoversClass(Jax::class)]
+#[CoversClass(Key::class)]
+#[CoversClass(Page::class)]
+#[CoversClass(Poll::class)]
+#[CoversClass(PrivateMessage::class)]
+#[CoversClass(ProfileTabs::class)]
+#[CoversClass(Reactions::class)]
+#[CoversClass(Request::class)]
+#[CoversClass(RequestStringGetter::class)]
+#[CoversClass(Router::class)]
+#[CoversClass(ServiceConfig::class)]
+#[CoversClass(Session::class)]
+#[CoversClass(Shoutbox::class)]
+#[CoversClass(SQLite::class)]
+#[CoversClass(Template::class)]
+#[CoversClass(TextFormatting::class)]
+#[CoversClass(TextRules::class)]
+#[CoversClass(Topic::class)]
+#[CoversClass(User::class)]
+#[CoversClass(UserProfile::class)]
+#[CoversClass(UsersOnline::class)]
 final class BadgesTest extends FeatureTestCase
 {
     protected function setUp(): void
