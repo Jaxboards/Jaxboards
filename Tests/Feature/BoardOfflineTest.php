4272868f6304216876a57156e89f6f5a57cd7f0e
Sean Johnson <seanjohnson08@gmail.com>
2025-12-14 00:54:11 -0500

    cleanup routing and add BoardOfflineTest

---
+++ Tests/Feature/BoardOfflineTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Jax\Attributes\Column;
+use Jax\Attributes\ForeignKey;
+use Jax\Attributes\Key;
+use Jax\BBCode;
+use Jax\BotDetector;
+use Jax\Database\Adapters\SQLite;
+use Jax\Database\Database;
+use Jax\Database\Model;
+use Jax\Database\Utils;
+use Jax\Date;
+use Jax\DebugLog;
+use Jax\DomainDefinitions;
+use Jax\FileSystem;
+use Jax\IPAddress;
+use Jax\Jax;
+use Jax\Modules\PrivateMessage;
+use Jax\Modules\Shoutbox;
+use Jax\Page;
+use Jax\Page\BoardOffline;
+use Jax\Request;
+use Jax\RequestStringGetter;
+use Jax\Router;
+use Jax\ServiceConfig;
+use Jax\Session;
+use Jax\Template;
+use Jax\TextFormatting;
+use Jax\TextRules;
+use Jax\User;
+use Jax\App;
+use Jax\Config;
+use PHPUnit\Framework\Attributes\CoversClass;
+use Tests\FeatureTestCase;
+
+use function DI\autowire;
+
+/**
+ * @internal
+ */
+#[CoversClass(App::class)]
+#[CoversClass(Column::class)]
+#[CoversClass(ForeignKey::class)]
+#[CoversClass(Key::class)]
+#[CoversClass(BBCode::class)]
+#[CoversClass(BotDetector::class)]
+#[CoversClass(Config::class)]
+#[CoversClass(SQLite::class)]
+#[CoversClass(Database::class)]
+#[CoversClass(Model::class)]
+#[CoversClass(Utils::class)]
+#[CoversClass(Date::class)]
+#[CoversClass(DebugLog::class)]
+#[CoversClass(DomainDefinitions::class)]
+#[CoversClass(FileSystem::class)]
+#[CoversClass(IPAddress::class)]
+#[CoversClass(Jax::class)]
+#[CoversClass(PrivateMessage::class)]
+#[CoversClass(Shoutbox::class)]
+#[CoversClass(Page::class)]
+#[CoversClass(BoardOffline::class)]
+#[CoversClass(Request::class)]
+#[CoversClass(RequestStringGetter::class)]
+#[CoversClass(Router::class)]
+#[CoversClass(ServiceConfig::class)]
+#[CoversClass(Session::class)]
+#[CoversClass(Template::class)]
+#[CoversClass(TextFormatting::class)]
+#[CoversClass(TextRules::class)]
+#[CoversClass(User::class)]
+final class BoardOfflineTest extends FeatureTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->container->set(
+            Config::class,
+            autowire()->constructorParameter('boardConfig', [
+                'boardoffline' => true,
+                'offlinetext' => 'pikachu',
+            ])
+        );
+    }
+
+    public function testViewBoardWhenBoardOfflineAsMember(): void
+    {
+        $this->actingAs('member');
+
+        $page = $this->go('index');
+
+        $this->assertStringContainsString("You don't have permission to view the board.", $page);
+        $this->assertStringContainsString("pikachu", $page);
+    }
+
+    public function testViewBoardWhenBoardOfflineAsAdmin(): void
+    {
+        $this->actingAs('admin');
+
+        $page = $this->go('index');
+
+        $this->assertStringNotContainsString("You don't have permission to view the board.", $page);
+    }
+}
