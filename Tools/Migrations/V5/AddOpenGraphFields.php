b9916996130120c05ff5546799c21ecafc102be4
Sean Johnson <seanjohnson08@gmail.com>
2025-12-17 03:06:02 -0500

    not sure why mysql requires parens here but OK

--- Tools/Migrations/V5/AddOpenGraphFields.php
+++ Tools/Migrations/V5/AddOpenGraphFields.php
@@ -11,7 +11,7 @@ final class AddOpenGraphFields
     public function execute(Database $database): void
     {
         $database->special(
-            "ALTER TABLE %t ADD `openGraphMetadata` JSON NOT NULL DEFAULT '{}'",
+            "ALTER TABLE %t ADD `openGraphMetadata` JSON NOT NULL DEFAULT ('{}')",
             ['posts'],
         );
     }
