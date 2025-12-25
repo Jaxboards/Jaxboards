71567e7cd9a241dcfdf370586a83395b6a4d4254
Sean Johnson <seanjohnson08@gmail.com>
2025-12-24 21:11:30 -0500

    forward slash file paths

--- Jax/GeoLocate.php
+++ Jax/GeoLocate.php
@@ -18,7 +18,7 @@ final readonly class GeoLocate
 
     public function __construct(FileSystem $fileSystem)
     {
-        $this->cityReader = new Reader($fileSystem->pathFromRoot('Service\maxmind\GeoLite2-City_20251223\GeoLite2-City.mmdb'));
+        $this->cityReader = new Reader($fileSystem->pathFromRoot('Service/maxmind/GeoLite2-City_20251223/GeoLite2-City.mmdb'));
     }
 
     public function lookup(string $ip): City
