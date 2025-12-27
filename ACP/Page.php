f7aba039983c84d78aa436938101a0c1c8627eff
Sean Johnson <seanjohnson08@gmail.com>
2025-12-27 07:22:27 -0500

    ACP -> twig

--- ACP/Page.php
+++ ACP/Page.php
@@ -7,6 +7,8 @@ namespace ACP;
 use Jax\DomainDefinitions;
 use Jax\FileSystem;
 use Jax\Request;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 use function array_key_exists;
 use function array_keys;
@@ -28,11 +30,28 @@ final class Page
         'title' => '',
     ];
 
+    private readonly Environment $twigEnvironment;
+
+    private readonly FilesystemLoader $filesystemLoader;
+
     public function __construct(
         private readonly DomainDefinitions $domainDefinitions,
         private readonly FileSystem $fileSystem,
         private readonly Request $request,
-    ) {}
+    ) {
+        $this->initializeTwig();
+    }
+
+    private function initializeTwig()
+    {
+        $this->filesystemLoader = new FilesystemLoader($this->fileSystem->pathFromRoot('ACP/views/'));
+        $this->twigEnvironment = new Environment($this->filesystemLoader, [
+            'cache' => $this->fileSystem->pathFromRoot('.cache/.twig.cache'),
+            // TODO: autoescaping should be turned on, but we previously did it manually
+            // and this is the easiest migration strategy at the moment
+            'autoescape' => false,
+        ]);
+    }
 
     public function append(string $partName, string $content): void
     {
@@ -145,9 +164,9 @@ final class Page
         string $templateFile,
         array $data = [],
     ): string {
-        $templateFile = 'ACP/views/' . $templateFile;
-        $fileInfo = $this->fileSystem->getFileInfo($templateFile);
 
+        // Add .html extension if needed
+        $fileInfo = $this->fileSystem->getFileInfo($templateFile);
         if ($fileInfo->getExtension() !== 'html') {
             if (mb_substr($templateFile, -1) !== '.') {
                 $templateFile .= '.';
@@ -156,12 +175,6 @@ final class Page
             $templateFile .= 'html';
         }
 
-        $template = $this->fileSystem->getContents($templateFile);
-
-        return str_replace(
-            array_map(static fn(string $name): string => '{{ ' . $name . ' }}', array_keys($data)),
-            array_map(static fn(float|int|string|null $content): string => "{$content}", $data),
-            $template,
-        ) . PHP_EOL;
+        return $this->twigEnvironment->render($templateFile . '.twig', $data);
     }
 }
