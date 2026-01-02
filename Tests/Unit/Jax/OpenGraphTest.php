5a51b83e3bad995093b6ab5783fee4e30de8f9c6
Sean Johnson <seanjohnson08@gmail.com>
2026-01-02 11:42:28 -0500

    ok

--- Tests/Unit/Jax/OpenGraphTest.php
+++ Tests/Unit/Jax/OpenGraphTest.php
@@ -56,7 +56,9 @@ final class OpenGraphTest extends UnitTestCase
                 <<<'HTML'
                     <html>
                     	<head>
-                    		<meta property="og:title" content="Dick Van Dyke is turning 100. Here’s what he’s been up to lately">
+                            <meta property="og:title" content="Dick Van Dyke is turning 100. Here’s what he’s been up to lately">
+                    		<!-- Some sites incorrectly use "name" attribute -->
+                    		<meta name="og:description" content="description">
                     		<meta property="og:type" content="website">
                     		<meta property="og:url" content="https://ogp.me/">
                     		<meta property="og:image" content="https://ogp.me/logo.png">
@@ -64,8 +66,6 @@ final class OpenGraphTest extends UnitTestCase
                     		<meta property="og:image:width" content="300">
                     		<meta property="og:image:height" content="300">
                     		<meta property="og:image:alt" content="The Open Graph logo">
-                    		<!-- Some sites incorrectly use "name" attribute -->
-                    		<meta name="og:description" content="description">
                     	</head>
                     </html>
                     HTML,
@@ -74,16 +74,16 @@ final class OpenGraphTest extends UnitTestCase
                 'https://github.com/Jaxboards/Jaxboards/releases/tag/3.0',
                 <<<HTML
                     <html>
-                    	<head>
+                        <head>
+                            <meta property="og:title" content="Release 3.0 · Jaxboards/Jaxboards">
+                    		<meta property="og:description" content="{ {$newlineDescription} }">
                     		<meta property="og:image" content="https://opengraph.githubassets.com/9e42864e715305b7fb43886e68810c7fa1ad5b45a971b6de80b1605b1983951b/Jaxboards/Jaxboards/releases/tag/3.0">
                     		<meta property="og:image:alt" content="{ {$newlineDescription} }">
                     		<meta property="og:image:width" content="1200">
                     		<meta property="og:image:height" content="600">
                     		<meta property="og:site_name" content="GitHub">
                     		<meta property="og:type" content="object">
-                    		<meta property="og:title" content="Release 3.0 · Jaxboards/Jaxboards">
                     		<meta property="og:url" content="/Jaxboards/Jaxboards/releases/tag/3.0">
-                    		<meta property="og:description" content="{ {$newlineDescription} }">
                     	</head>
                     </html>
                     HTML,
