0f41ecfb6083c1bd934c48b65a2d2ce1f0c82e39
Sean Johnson <seanjohnson08@gmail.com>
2026-01-04 02:51:43 -0500

    of course the very first thing I test is timezone dependent

--- Script/components/idle-clock.test.ts
+++ Script/components/idle-clock.test.ts
@@ -1,8 +1,13 @@
 import IdleClock from './idle-clock';
 
 test('idle clock', function () {
-    const twoThirtyAM = 1767512040;
-    document.body.innerHTML = `<div class="idle lastAction${twoThirtyAM}">Sean</div>`;
+    const twoThirtyAM = new Date();
+    twoThirtyAM.setHours(2);
+    twoThirtyAM.setMinutes(30);
+
+    const twoThirtyMS = Math.floor(twoThirtyAM.valueOf() / 1000);
+
+    document.body.innerHTML = `<div class="idle lastAction${twoThirtyMS}">Sean</div>`;
 
     IdleClock.hydrate(document.body);
 
