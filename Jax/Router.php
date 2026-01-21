1d9a2d14bb157706acc645498bd60598fa8e450c
Sean Johnson <seanjohnson08@gmail.com>
2026-01-21 17:01:50 -0500

    switch to manifest.json

--- Jax/Router.php
+++ Jax/Router.php
@@ -82,7 +82,7 @@ final class Router
         $this->get('members', '/members', Members::class);
         $this->get('modcontrols', '/modcontrols/{do}', ModControls::class);
         $this->get('forum', '/forum/{id}/{slug}', Forum::class);
-        $this->get('manifest', '/manifest', Manifest::class);
+        $this->get('manifest.json', '/manifest.json', Manifest::class);
         $this->get('post', '/post', Post::class);
         $this->get('profile', '/profile/{id}/{page}', UserProfile::class);
         $this->get('rainbow', '/rainbow', Rainbow::class);
