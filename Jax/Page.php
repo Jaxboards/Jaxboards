b781a5b4b21e66619424a0b31a408c49f71b4342
Sean Johnson <seanjohnson08@gmail.com>
2026-02-07 21:16:25 -0500

    cache bust

--- Jax/Page.php
+++ Jax/Page.php
@@ -214,7 +214,7 @@ final class Page
                         rel="preload"
                         as="style"
                         type="text/css"
-                        href="/Service/Themes/global.css"
+                        href="/Service/Themes/global.css?1770516969"
                         onload="this.onload=null;this.rel='stylesheet'"
                     >
                     <link rel="stylesheet" type="text/css" href="{$themeUrl}">
